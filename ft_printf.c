0945de33dcc4d1735e0d079f5850bd980c6fc2c7
lzaktas <zerennaktas@gmail.com>
2026-08-26 22:48:05 +0000

    c1

--- ft_printf.c
+++ ft_printf.c
@@ -1,20 +1,39 @@
 #include "ft_printf.h"
 
-int ft_format_control(char fs, va_list args)
+static int  ft_format_control(char fs, va_list args)
 {
     int i;
 
     i = 0;
     if (fs == 'c')
-    else if (fs == 'd' ||fs == 'i')
+        return (ft_putchar(va_arg(*args, int)));
+    else if (fs == 'd' || fs == 'i')
+        return (ft_putnbr(va_arg(*args, int)));
     else if (fs == 's')
+        return (ft_putstr(va_arg(*args, char *)));
     else if (fs == 'p')
+        return (ft_putptr(va_arg(*args, int)));
     else if (fs == 'u')
+        return (ft_putunbr(va_arg(*args, int)));
     else if (fs == 'x')
-    else if (fs == 'X')  
+        return (ft_puthex(va_arg(*args, int)));
+    else if (fs == 'X') 
+        return (ft_puthex(va_arg(*args, int))) ;
     else if (fs == '%')
-        i += write(1, "%", 1);
+        return (ft_putchar('%'));
     return (i);
 }
 
-BİLGİSAYARI KARIŞTIRMA
+
+int ft_printf(const char*, ...)
+{
+
+}
+/*
+int ft_putchar(va_list,args)
+{
+    char a;
+    a = (char)va_arg(args, int);
+    return (write(1, &a, 1));
+}
+*/
