cceb0831e79ebf3decd002ae2dcc27f36797d277
lzaktas <zerennaktas@gmail.com>
2026-09-05 02:04:09 +0300

    change the rtn

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -14,15 +14,15 @@
 
 int	ft_putnbr_base(unsigned long long nb, char *base, int base_len)
 {
-	int	count;
+	int	rtn;
 
-	count = 0;
+	rtn = 0;
 	if (nb >= (unsigned long long) base_len)
 	{
-		count += ft_putnbr_base((nb / base_len), base, base_len);
-		count += ft_putnbr_base((nb % base_len), base, base_len);
+		rtn += ft_putnbr_base((nb / base_len), base, base_len);
+		rtn += ft_putnbr_base((nb % base_len), base, base_len);
 	}
 	else
-		count += write(1, &base[nb], 1);
-	return (count);
+		rtn += write(1, &base[nb], 1);
+	return (rtn);
 }
